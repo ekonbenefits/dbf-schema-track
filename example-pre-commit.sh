adb8178fff982565cf026fb465a460a68b0e6fae
jbtule <jay+code@tuley.name>
2017-02-10 15:23:32 -0600

    added ignore read error to pre commit check
    fixes #1

--- example-pre-commit.sh
+++ example-pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-</your/path/>dbf-schema-track.exe <DBFNAME1> <DBFNAME2>
+</your/path/>dbf-schema-track.exe --ignore-read-error <DBFNAME1> <DBFNAME2>
 RESULT=$?
 [ $RESULT -ne 0 ] && exit 1
 exit 0
